987d146bc0aef4543619fca4dc930b90725429bb
huapuer <ucman@126.com>
2017-06-23 23:22:10 +0800

    modify net test case

--- Alan/net.h
+++ Alan/net.h
@@ -12,8 +12,7 @@ IGUIEnvironment* guienv;
 void acts_test(char* c, int size) {
 	guienv->clear();
 	guienv->addStaticText(L"From Friedrich£º", rect<s32>(10, 10, 260, 22), true);
-	int bufSize = MultiByteToWideChar(CP_ACP, 0, c, -1, NULL, 0);
-	wchar_t* wp = new wchar_t[bufSize];
-	MultiByteToWideChar(CP_ACP, 0, c, -1, wp, bufSize);
+	wchar_t* wp = new wchar_t[size];
+	MultiByteToWideChar(CP_ACP, 0, c, -1, wp, size);
 	guienv->addStaticText(wp, rect<s32>(70, 10, 260, 22), true);
 }
